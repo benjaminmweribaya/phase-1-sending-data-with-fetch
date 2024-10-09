f7382ec9f00cfc42f2c8aba32384c8f137165ac2
benjaminmweribaya <b3njaminbaya@gmail.com>
2024-10-09 21:35:51 +0300

    Sending Data with Fetch

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Add your code here
+function submitData(name, email) {
+    const userData = {
+        name: name,
+        email: email
+    }
+
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(userData)
+    })
+
+        .then(response => {
+            return response.json();
+        })
+
+        .then(data => {
+            const body = document.querySelector("body");
+            const newId = document.createElement("p");
+            newId.textContent = `New User ID: ${data.id}`;
+            body.appendChild(newId);
+        })
+
+        .catch(error => {
+            const body = document.querySelector("body");
+            const errorMessage = document.createElement("p");
+            errorMessage.textContent = `Error: ${error.message}`;
+            body.appendChild(errorMessage);
+        });
+
+}
